0066232ade7dd57385f8f545990e6eebf62c3535
DerpfacePython <DerpfacePython@users.noreply.github.com>
2015-10-04 10:24:03 +0800

    Updated game.py to V0.2.1
    Fully fixed the savefile, and fixed most of the bugs.

--- game.py
+++ game.py
@@ -5,45 +5,9 @@ import time
 master = Tk()
 master.title("Ways To NOT Earn Money")
 
-#VARIABLES
-upgcheck1 = int(0)
-upgcheck2 = int(0)
-upgcheck3 = int(0)
-clickupgcheck1 = int(0)
-inc = int(1)
-inctkinter = StringVar()
-inctkinter.set("+" + str(inc) + " money!")
-money = int(0)
-moneytkinter = StringVar()
-moneytkinter.set("Balance: $" + str(money))
-autoclick = int(0)
-autoclick2 = int(0)
-autoclicktkinter = StringVar()
-autoclicktkinter.set("Auto-Clickers Amount: " + str(int(autoclick2*10)))
-autoprice = int(20*(math.pow(1.1,int(autoclick2*10))))
-autopricetkinter = StringVar()
-autopricetkinter.set("Auto-Clicker (Costs: $" + str(autoprice) + ")")
-printmoney = int(0)
-printmoney2 = int(0)
-printmoneytkinter = StringVar()
-printmoneytkinter.set("Money Printers Amount: " + str(int(float(printmoney2*10)/15)))
-printprice = int(275*(math.pow(1.1,int(printmoney2*10))))
-printpricetkinter = StringVar()
-printpricetkinter.set("Money Printer (Costs: $" + str(printprice) + ")")
-counterfeit = int(0)
-counterfeit2 = int(0)
-counterfeittkinter = StringVar()
-counterfeittkinter.set("Counterfeit Companies Amount: " + str(int(float(counterfeit2*10)/221)))
-counterfeitprice = int(9001*(math.pow(1.1,int(counterfeit2*10))))
-counterfeitpricetkinter = StringVar()
-counterfeitpricetkinter.set("Counterfeit Company (Costs: $" + str(counterfeitprice) + ")")
-mps = int(0)
-mpstkinter = StringVar()
-mpstkinter.set("MPS: " + str(mps))
-
 #AUTO CLICKER
 def boostauto1():
-    global money, autoclick, autoclick2
+    global money, autoclick, autoclick2, upgcheck1
     if money < 5000 or autoclick2 == 0:
         print("You do not meet the requirements.")
     elif money >= 5000 and autoclick2 > 0:
@@ -51,7 +15,13 @@ def boostauto1():
         autoclick = int(autoclick*30)/10
         upgcheck1 = upgcheck1 + 1
         boostbutton1.destroy()
-        boostbutton2.grid(row=2, column=4, sticky=E)
+        if upgcheck2 == int(1) and upgcheck3 == int(0):
+            boostbutton3.grid(row=1, column=4, sticky=E)
+        elif upgcheck2 == int(0) and upgcheck3 == int(1):
+            boostbutton2.grid(row=1, column=4, sticky=E)
+        else:
+            boostbutton2.grid(row=1, column=4, sticky=E)
+            boostbutton3.grid(row=2, column=4, sticky=E)
 
 def deduction1():
     global money, autoclick, autoclick2, autoprice, counterfeit, printmoney, mps
@@ -59,12 +29,12 @@ def deduction1():
         print("You cannot afford this.")
     elif money >= int(autoprice):
         money = money - int(autoprice)
-        autoclick = autoclick + 0.1
+        autoclick = autoclick + 0.1*(1 + upgcheck1*2)
         autoclick2 = autoclick2 + 0.1
         mps = mps + 1
         mpstkinter.set("MPS: " + str(mps))
         autoclicktkinter.set("Auto-Clickers Amount: " + str(int(autoclick2*10)))
-        autoprice = int(20*(math.pow(1.1,int(autoclick2*10))))
+        autoprice = int(20*(math.pow(1.25,int(autoclick2*10))))
         autopricetkinter.set("Auto-Clicker (Costs: $" + str(autoprice) + ")")
         if autoclick == 0.1 and counterfeit == 0 and printmoney == 0:
             automoney()
@@ -79,6 +49,12 @@ def boostauto2():
         autoclick = int(printmoney*30)/10
         upgcheck2 = upgcheck2 + 1
         boostbutton2.destroy()
+        if upgcheck1 == int(1) and upgcheck3 == int(0):
+            boostbutton3.grid(row=1, column=4, sticky=E)
+        elif upgcheck3 == int(1):
+            pass
+        else:
+            boostbutton3.grid(row=2, column=4, sticky=E)
 
 def deduction2():
     global money, printmoney, printmoney2, printprice, mps, counterfeit
@@ -91,9 +67,9 @@ def deduction2():
         mps = mps + 15
         mpstkinter.set("MPS: " + str(mps))
         printmoneytkinter.set("Money Printers Amount: " + str(int(float(printmoney2*10)/15)))
-        printprice = int(275*(math.pow(1.1,float(int(printmoney2*10)/15))))
+        printprice = int(275*(math.pow(1.25,float(int(printmoney2*10)/15))))
         printpricetkinter.set("Money Printer (Costs: $" + str(printprice) + ")")
-        if printmoney == 0.75 and counterfeit == 0 and autoclick == 0:
+        if printmoney == 1.5 and counterfeit == 0 and autoclick == 0:
             automoney()
 
 #COUNTERFEIT COMPANY
@@ -108,7 +84,7 @@ def boostauto3():
         boostbutton3.destroy()
 
 def deduction3():
-    global money, countefeit, counterfeit2, countefeitprice, printmoney, automoney, mps
+    global money, counterfeit, counterfeit2, counterfeitprice, printmoney, automoney, mps
     if money < int(autoprice):
         print("You cannot afford this.")
     elif money >= int(autoprice):
@@ -117,9 +93,9 @@ def deduction3():
         counterfeit2 = counterfeit2 + 22.1
         mps = mps + 221
         mpstkinter.set("MPS: " + str(mps))
-        counterfeittkinter.set("Auto-Clickers Amount: " + str(int(float(counterfeit2*10)/221)))
-        counterfeitprice = int(20*(math.pow(1.1,int(countefeit2*10))))
-        counterfeitpricetkinter.set("Auto-Clicker (Costs: $" + str(counterfeit) + ")")
+        counterfeittkinter.set("Counterfeit Companies Amount: " + str(int(float(counterfeit2*10)/221)))
+        counterfeitprice = int(9001*(math.pow(1.25,float(int(counterfeit2*10)/221))))
+        counterfeitpricetkinter.set("Counterfeit Company (Costs: $" + str(counterfeitprice) + ")")
         if counterfeit == 22.1 and printmoney == 0 and automoney == 0:
             automoney()
         
@@ -138,41 +114,52 @@ def clickboost1():
         inc = inc + 2
         inctkinter.set("+" + str(inc) + " money!")
         clickupgcheck1 = clickupgcheck1 + 1
-        clickboost1.destroy()
-        boostbutton1.grid(row=1, column=4, sticky=E)
-        boostbutton2.grid(row=2, column=4, sticky=E)
+        clickbooster1.destroy()
+        if upgcheck1 == int(1) and upgcheck2 == int(1):
+            boostbutton3.grid(row=1, column=4, sticky=E)
+        elif upgcheck1 == int(1) and upgcheck2 == int(0):
+            boostbutton2.grid(row=1, column=4, sticky=E)
+            boostbutton3.grid(row=2, column=4, sticky=E)
+        elif upgcheck1 == int(0) and upgcheck2 == int(1):
+            boostbutton3.grid(row=3, column=4, sticky=E)
+        else:
+            boostbutton1.grid(row=1, column=4, sticky=E)
+            boostbutton2.grid(row=2, column=4, sticky=E)
+            boostbutton3.grid(row=3, column=4, sticky=E)
 
 #AUTOMATIC MONEY
 def automoney():
-    global money, automoney, autoprice, autoclick, printmoney, printprice, counterfeit, counterfeitprice
-    money = money + (autoclick + printmoney + counterfeit)
-    round(money, 2)
+    global money, autoclick, autoclick2, autoprice, printmoney, printmoney2, printprice, counterfeit, counterfeit2, counterfeitprice, mps
+    #BUG FIXER
     while mps > 10*(autoclick + printmoney + counterfeit):
         if mps - 221 >= 10*(autoclick + printmoney + counterfeit):
-            counterfeit = counterfeit + 22.1
+            counterfeit = counterfeit + 22.1*(1 + upgcheck3*2)
+            counterfeit2 = counterfeit2 + 22.1
             counterfeittkinter.set("Counterfeit Companies Amount: " + str(int(float(counterfeit2*10)/221)))
-            counterfeitprice = int(9001*math.pow(1.1,float(int(counterfeitprice*10)/221)))
+            counterfeitprice = int(9001*(math.pow(1.25,int(float(counterfeit2*10)/221))))
             counterfeitpricetkinter.set("Counterfeit Company (Costs: $" + str(counterfeitprice) + ")")
             continue
-        elif mps - 221 < 10*(autoclick + printmoney + counterfeit) and mps - 15 >= 10*(autoclick + printmoney + counterfeit):
-            printmoney = printmoney + 1.5        
+        elif mps - 15 >= 10*(autoclick + printmoney + counterfeit):
+            printmoney = printmoney + 1.5*(1 + upgcheck2*2)
+            printmoney2 = printmoney2 + 1.5
             printmoneytkinter.set("Money Printers Amount: " + str(int(float(printmoney2*10)/15)))
-            printprice = int(275*(math.pow(1.1,float(int(printmoney2*10)/15))))
+            printprice = int(275*(math.pow(1.25,int(float(printmoney2*10)/15))))
             printpricetkinter.set("Money Printer (Costs: $" + str(printprice) + ")")
             continue
-        elif mps - 15 < 10*(autoclick + printmoney):
-            for a in range(int((10*(autoclick + printmoney + counterfeit))), mps):
-                autoclick = autoclick + 0.1
-                autoclicktkinter.set("Auto-Clickers Amount: " + str(int(autoclick2*10)))
-                autoprice = int(20*(math.pow(1.1,int(autoclick2*10))))
-                autopricetkinter.set("Auto-Clicker (Costs: $" + str(autoprice) + ")")
+        else:
+            autoclick = autoclick + 0.1*(1 + upgcheck1*2)
+            autoclick2 = autoclick2 + 0.1
+            autoclicktkinter.set("Money Printers Amount: " + str(int(autoclick2*10)))
+            autoprice = int(275*(math.pow(1.25,int(autoclick2*10))))
+            autopricetkinter.set("Money Printer (Costs: $" + str(autoprice) + ")")
             continue
     moneytkinter.set("Balance: $" + str(money))
+    money = money + float(mps)/10
     master.after(100, automoney)
 
 #SAVING GAME
 def savegame():
-    x = ["auto", int(float(autoclick2)*10), "print", int(float(int(printmoney2*10))/15), "counter", int(float(int(counterfeit2))/221), \
+    x = ["auto", int(float(autoclick2)*10), "print", int(float(int(printmoney2*10))/15), "counter", int(float(int(counterfeit2*10))/221), \
          "upg1", int(upgcheck1), "upg2", int(upgcheck2), "upg3", int(upgcheck3), "cupg1", int(clickupgcheck1), "money", int(money)]
     savefile = (str("_".join(str(v) for v in x))).encode("hex")
     f = open("savefile.txt", "w")
@@ -180,32 +167,73 @@ def savegame():
     f.close()
     toplevel1 = Toplevel()
     msg = Message(toplevel1, text="Game saved!")
-    
-#SAVE IMPORTS
+    msg.pack()
+
+#SAVE IMPORTS AND VARIABLES
 g = open("savefile.txt")
 g2 = str(g.read())
 g2 = (g2.decode("hex")).split("_")
+upgcheck1 = int(g2[7])
+upgcheck2 = int(g2[9])
+upgcheck3 = int(g2[11])
+clickupgcheck1 = int(g2[13])
 money = int(g2[15])
+moneytkinter = StringVar()
 moneytkinter.set("Balance: $" + str(money))
-autoclick = int((int(float(g2[1])/10)*int(2*g2[7])) + int(float(g2[1])/10))
-autoclick2 = int(float(g2[1])/10)
-autoclicktkinter.set("Auto-Clickers Amount: " + str(int(g2[1])))
-autoprice = int(20*(math.pow(1.1,int(g2[1]))))
-autopricetkinter.set("Auto-Clicker (Costs: $" + str(int(20*(math.pow(1.1,int(g2[1]))))) + ")")
-printmoney = int((int(float(g2[3])/10)*int((2*g2[9]))) + int(float(g2[3])/10))
-printmoney2 = int(float(g2[3])/10)
-printmoneytkinter.set("Money Printers Amount: " + str(int(float(g2[3])/15)))
-printprice = int(275*(math.pow(1.1,int(g2[3]))))
-printpricetkinter.set("Money Printer (Costs: $" + str(int(275*(math.pow(1.1,int(g2[3]))))) + ")")
-counterfeit = int((int(float(g2[5])/10)*int((2*g2[11]))) + int(float(g2[5])/10))
-counterfeit2 = int(float(g2[5])/10)
-counterfeittkinter.set("Counterfeit Companies Amount: " + str(int(float(g2[5])/221)))
-counterfeitprice = int(9001*(math.pow(1.1,int(g2[5]*10))))
-counterfeitpricetkinter.set("Counterfeit Company (Costs: $" + str(int(9001*(math.pow(1.1,int(g2[5]*10))))) + ")")
+autoclick = ((float(g2[1])/10)*int(2*g2[7])) + (float(g2[1])/10)
+autoclick2 = float(g2[1])/10
+autoclicktkinter = StringVar()
+autoclicktkinter.set("Auto-Clickers Amount: " + str(g2[1]))
+autoprice = int(20*(math.pow(1.25,int(g2[1]))))
+autopricetkinter = StringVar()
+autopricetkinter.set("Auto-Clicker (Costs: $" + str(autoprice) + ")")
+printmoney = (float(int(g2[3])*15)/10)*int((2*g2[9])) + (float(int(g2[3])*15)/10)
+printmoney2 = float(int(g2[3])*15)/10
+printmoneytkinter = StringVar()
+printmoneytkinter.set("Money Printers Amount: " + str(g2[3]))
+printprice = int(275*(math.pow(1.25,int(g2[3]))))
+printpricetkinter = StringVar()
+printpricetkinter.set("Money Printer (Costs: $" + str(printprice) + ")")
+counterfeit = (float(int(g2[5])*221)/10)*int((2*g2[11])) + (float(int(g2[5])*221)/10)
+counterfeit2 = int(float(int(g2[5])*221)/10)
+counterfeittkinter = StringVar()
+counterfeittkinter.set("Counterfeit Companies Amount: " + str(g2[5]))
+counterfeitprice = int(9001*(math.pow(1.25,int(g2[5]*10))))
+counterfeitpricetkinter = StringVar()
+counterfeitpricetkinter.set("Counterfeit Company (Costs: $" + str(counterfeitprice) + ")")
 inc = int(1 + (int(g2[13])*2))
-inctkinter.set("+" + str(int(1 + int(g2[13])*2)) + " money!")
-mps = int(float(int(g2[1] + g2[3] + g2[5]))/100)
-mpstkinter.set("MPS: " + str(int(float(int(g2[1] + g2[3] + g2[5]))/100)))
+inctkinter = StringVar()
+inctkinter.set("+" + str(inc) + " money!")
+mps = int(g2[1]) + 15*int(g2[3]) + 221*int(g2[5])
+mpstkinter = StringVar()
+mpstkinter.set("MPS: " + str(mps))
+if g2[7] == int(1):
+    if upgcheck2 == int(1) and upgcheck3 == int(0):
+        boostbutton3.grid(row=1, column=4, sticky=E)
+    elif upgcheck3 == int(1):
+        boostbutton2.grid(row=1, column=4, sticky=E)
+    else:
+        boostbutton2.grid(row=1, column=4, sticky=E)
+        boostbutton3.grid(row=2, column=4, sticky=E)
+if g2[9] == int(1):
+    if upgcheck1 == int(1) and upgcheck3 == int(0):
+        boostbutton3.grid(row=1, column=4, sticky=E)
+    elif upgcheck1 == int(1) and upgcheck3 == int(1):
+        pass
+    else:
+        boostbutton3.grid(row=2, column=4, sticky=E)
+if g2[13] == int(1):
+    if upgcheck1 == int(1) and upgcheck2 == int(1):
+        boostbutton3.grid(row=1, column=4, sticky=E)
+    elif upgcheck1 == int(1) and upgcheck2 == int(0):
+        boostbutton2.grid(row=1, column=4, sticky=E)
+        boostbutton3.grid(row=2, column=4, sticky=E)
+    elif upgcheck1 == int(0) and upgcheck2 == int(1):
+        boostbutton3.grid(row=3, column=4, sticky=E)
+    else:
+        boostbutton1.grid(row=1, column=4, sticky=E)
+        boostbutton2.grid(row=2, column=4, sticky=E)
+        boostbutton3.grid(row=3, column=4, sticky=E)    
 automoney()
 
 #BUTTONS, LABELS AND ENTRIES
@@ -255,6 +283,6 @@ boostbutton3 = Button(master, text="Counterfeit Company Boost (Costs: $2133748)"
 boostbutton3.grid(row=4, column=4, sticky=E)
 
 savebutton = Button(master, text="Save game", width=10, command=savegame)
-savebutton.grid(row=5, column=4, sticky=E)
+savebutton.grid(row=6, column=4, sticky=E)
 
 mainloop()
