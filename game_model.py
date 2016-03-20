e14bfe8dc395d3c292408149711763e0c55a3c9d
Derp McDerpson <DerpfacePython@users.noreply.github.com>
2016-03-20 20:32:31 +1100

    Game_Model!

--- game_model.py
+++ game_model.py
@@ -31,27 +31,54 @@ class GameState:
         self.autoclick2 = int(self.data[1])
         self.autoclick = self.autoclick2 * 18 * self.upgcheck1h2 + self.autoclick2 * 2 * self.upgcheck1h1 + \
                          self.autoclick2
-        self.autoprice = int(20 * math.pow(1.2, self.autoclick2))
+        self.autoprice = int(20 * math.pow(1.15, self.autoclick2))
 
         self.printmoney2 = int(self.data[3])
         self.printmoney = self.printmoney2* 18 * self.upgcheck2h2 + self.printmoney2 * 2 * self.upgcheck2h1 + \
                           self.printmoney2
-        self.printprice = int(375 * (math.pow(1.2, self.printmoney2)))
+        self.printprice = int(375 * math.pow(1.25, self.printmoney2))
 
         self.counterfeit2 = int(self.data[5])
         self.counterfeit = self.counterfeit2 * 2 * self.upgcheck3 + self.counterfeit2
-        self.counterfeitprice = int(9001 * math.pow(1.2, self.counterfeit2))
+        self.counterfeitprice = int(9001 * math.pow(1.3, self.counterfeit2))
 
         self.sharecrash2 = int(self.data[7])
         self.sharecrash = self.sharecrash2 * 2 * self.upgcheck4 + self.sharecrash2
-        self.shareprice = int(42000 * math.pow(1.2, self.sharecrash2))
+        self.shareprice = int(42000 * math.pow(1.4, self.sharecrash2))
 
         self.bankheist2 = int(self.data[7])
         self.bankheist = self.bankheist2 * 2 * self.upgcheck4 + self.bankheist2
-        self.bankprice = int(181700 * math.pow(1.2, self.bankheist2))
+        self.bankprice = int(175000 * math.pow(1.1, self.bankheist2))
 
         self.mps = self.autoclick2 + 15 * self.printmoney2 + 321 * self.counterfeit2 + 969 * self.sharecrash2
         self.inc = 1 + self.clickupgcheck1 * 2 + self.clickupgcheck2 * self.mps / 10
+
+        self.multiplier = 1
+
+    def multiplierset(self, n):
+        if n == "1":
+            self.autoprice = int(20 * math.pow(1.15, self.autoclick2))
+            self.printprice = int(375 * math.pow(1.25, self.printmoney2))
+            self.counterfeitprice = int(9001 * math.pow(1.3, self.counterfeit2))
+            self.shareprice = int(42000 * math.pow(1.4, self.sharecrash2))
+            self.bankprice = int(175000 * math.pow(1.1, self.bankheist2))
+            self.multiplier = 1
+        elif n == "10":
+            for a in range(1, 10):
+                self.autoprice += int(20 * math.pow(1.15, (self.autoclick2 + a)))
+                self.printprice += int(375 * math.pow(1.25, (self.printmoney2 + a)))
+                self.counterfeitprice += int(9001 * math.pow(1.3, (self.counterfeit2 + a)))
+                self.shareprice += int(42000 * math.pow(1.4, (self.sharecrash2 + a)))
+                self.bankprice += int(175000 * math.pow(1.1, (self.bankheist2 + a)))
+                self.multiplier = 10
+        elif n == "100":
+            for a in range(1, 100):
+                self.autoprice += int(20 * math.pow(1.15, (self.autoclick2 + a)))
+                self.printprice += int(375 * math.pow(1.25, (self.printmoney2 + a)))
+                self.counterfeitprice += int(9001 * math.pow(1.3, (self.counterfeit2 + a)))
+                self.shareprice += int(42000 * math.pow(1.4, (self.sharecrash2 + a)))
+                self.bankprice += int(175000 * math.pow(1.1, (self.bankheist2 + a)))
+                self.multiplier = 10
 
     def set(self, name, value):
         i = self.data.index(name)
@@ -120,10 +147,10 @@ class GameState:
     def inc_counterfeit2(self, amount=1):
         self.counterfeit2 += amount
 
-    def get_counterfeit_price(self):
+    def get_counterfeitprice(self):
         return self.counterfeitprice
 
-    def set_counterfeit_price(self, amount):
+    def set_counterfeitprice(self, amount):
         self.counterfeitprice = amount
 
     def get_sharecrash(self):
@@ -192,6 +219,9 @@ class GameState:
     def set_inc(self, amount):
         self.inc = amount
 
+    def get_multiplier(self):
+        return self.multiplier
+
     def get_total_clicks(self):
         return self.totalclicks
 
@@ -203,6 +233,21 @@ class GameState:
 
     def inc_timeplay(self):
         self.timeplay += 1
+
+    def get_totalspent(self):
+        x = 0
+        for a in range(0, self.get_autoclick2()):
+            x += int(20 * math.pow(1.15, self.autoclick2))
+        for b in range(0, self.get_printmoney2()):
+            x += int(375 * math.pow(1.25, self.printmoney2))
+        for c in range(0, self.get_counterfeit2()):
+            x += int(9001 * math.pow(1.3, self.counterfeit2))
+        for d in range(0, self.get_sharecrash2()):
+            x += int(42000 * math.pow(1.4, self.sharecrash2))
+        for e in range(0, self.get_bankheist2()):
+            x += int(175000 * math.pow(1.1, self.bankheist2))
+        x += self.get_total_clicks()
+        return x
 
     def get_money(self):
         return self.money
