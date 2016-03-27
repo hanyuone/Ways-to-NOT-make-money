f38c16d3bfe9377105fbba066982935f1112613b
qlstudio <qlstudio@gmail.com>
2016-03-27 13:05:26 -0400

    Derpy's request (Don't merge!!!!)

--- save_and_load.py
+++ save_and_load.py
@@ -14,7 +14,7 @@ def encode_and_save(username, data):
         data = ["auto", 0, "print", 0, "counter", 0, "shares", 0, "bank", 0, "upg1h1", 0, "upg1h2", 0,
                 "upg2h1", 0, "upg2h2", 0, "upg3", 0, "upg4", 0, "upg5", 0, "cupg1", 0, "cupg2", 0,
                 "quintillion", 0, "quadrillion", 0, "trillion", 0, "billion", 0, "million", 0,
-                "money", 0.0, "time", 0, "clicks", 0]
+                "money", 0.0, "time", 0, "clicks", 0, 'lotto', 100]
 
     encoded_data = ("_".join(str(v) for v in data)).encode("hex") + ";"
 
